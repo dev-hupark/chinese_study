fedaf886d41b120584f5ca317fe303a723a0b753
hupark <dev.hupark@gmail.com>
2025-04-29 20:19:51 +0900

    error 확인 로그 추가

--- src/hooks/useUserInfo.js
+++ src/hooks/useUserInfo.js
@@ -8,6 +8,7 @@ export const useUserInfo = () => {
   useEffect(() => {
     const fetchUserInfo = async () => {
       try {
+        const { data: { session } } = await client.auth.getSession();
         const { data: { user }, error: userError } = await client.auth.getUser();
 
         if (userError || !user) {
@@ -18,19 +19,23 @@ export const useUserInfo = () => {
         }
 
         const { data, error } = await client
-          .from('users')
-          .select('*')
-          .eq('id', user.id)
-          .single();
+            .from('users')
+            .select('*')
+            .eq('id', user.id)
+            .single();
+
+        console.log(user.id);
 
         if (error) {
           console.error('users 테이블 조회 실패:', error);
+
           const user = await client.auth.getUser();
           console.log('auth 유저 정보 ', user);
           setUserInfo(null);
         } else {
           setUserInfo(data);
         }
+
       } catch (e) {
         console.error('알 수 없는 에러:', e);
         setUserInfo(null);
