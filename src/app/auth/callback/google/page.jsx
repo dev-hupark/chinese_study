41dcde0f557e0fab3dba36777e3cc2efac7cf803
hupark <dev.hupark@gmail.com>
2025-04-29 20:23:33 +0900

    error 확인 로그 추가

--- src/app/auth/callback/google/page.jsx
+++ src/app/auth/callback/google/page.jsx
@@ -6,15 +6,16 @@ import { useEffect } from 'react'
 
 export default function AuthCallbackPage() {
     const router = useRouter()
+    console.log('AuthCallbackPage');
     useSyncUser()
 
-    useEffect(() => {
+   /* useEffect(() => {
         const timer = setTimeout(() => {
             router.push('/')  // 로그인 후 메인으로 이동
         }, 1000)
 
         return () => clearTimeout(timer)
-    }, [])
+    }, [])*/
 
     return <div>로그인 처리 중입니다...</div>
 }
